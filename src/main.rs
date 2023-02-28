ef952cb8ada7ce0ace1bda5063e96449ac662365
Ying Liu <yingliu4203@gmail.com>
2023-02-28 11:39:22 -0800

    secrecy and request_id

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -10,7 +11,7 @@ async fn main() -> std::io::Result<()> {
     init_tracing("zero2prod".into(), "info".into(), std::io::stdout);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let pool = PgPool::connect(&configuration.database.connection_string())
+    let pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
         .await
         .expect("Failed to connect to PG.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
